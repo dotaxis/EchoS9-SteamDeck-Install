62cacf5f6a6eceef05b1beacc6b2b999128a71a3
Chase Taylor <x8@live.ca>
2023-08-28 18:21:18 -0600

    Set FF9 to run with Proton Experimental

--- run-installer.sh
+++ run-installer.sh
@@ -6,6 +6,7 @@ echo
 
 echo "Downloading & Installing .NET Desktop 6.0 Runtimes"
 flatpak run com.github.Matoking.protontricks 377840 dotnetdesktop6 &> /dev/null
+echo
 
 echo "Downloading Echo-S 9 Installer"
 curl -sSL -o "EchoS9.tmp.exe" "https://download.tsunamods.com/?id=7"
@@ -15,5 +16,19 @@ STEAM_COMPAT_CLIENT_INSTALL_PATH="${HOME}/.local/share/Steam/" \
 STEAM_COMPAT_DATA_PATH="${HOME}/.local/share/Steam/steamapps/compatdata/377840/" \
 ${HOME}/.local/share/Steam/steamapps/common/Proton\ 8.0/proton run EchoS9.tmp.exe &> /dev/null
 rm EchoS9.tmp.exe
+echo
+
+echo "Forcing Final Fantasy IX to run under Proton Experimental"
+kill $(ps aux | grep '[s]team -steamdeck' | awk '{print $2}')
+sleep 10
+cp ${HOME}/.local/share/Steam/config/config.vdf ${HOME}/.local/share/Steam/config/config.vdf.bak
+perl -0777 -i -pe 's/"CompatToolMapping"\n\s+{/"CompatToolMapping"\n\t\t\t\t{\n\t\t\t\t\t"377840"\n\t\t\t\t\t{\n\t\t\t\t\t\t"name"\t\t"proton_experimental"\n\t\t\t\t\t\t"config"\t\t""\n\t\t\t\t\t\t"priority"\t\t"250"\n\t\t\t\t\t}/gs' \
+${HOME}/.local/share/Steam/config/config.vdf
+# Thanks ChatGPT
+nohup steam &> /dev/null &
+echo
 
 echo "Done! Launch Final Fantasy IX to select mods."
+echo "Press enter to exit."
+read
+kill -9 $PPID
