5f2f0113057ea47b170ce03dc4e453d40c598715
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-04-30 10:00:03 -0600

    Stop forcing Proton Experimental

--- README.md
+++ README.md
@@ -6,6 +6,5 @@ Bash script to run the Echo-S 9 installer on Steam Deck
 2. Right-click and select "Run in Konsole"
 3. Put `Z:/home/deck/.local/share/Steam/steamapps/common/FINAL FANTASY IX/` in the text box (if game is installed to internal drive)
 4. Select Moguri Mod (or don't -- your call)
-5. Click Install
-6. Wait for Steam to restart
-7. Run Final Fantasy IX from Steam and enjoy!
+5. Click Install and wait
+6. Run Final Fantasy IX from Steam and enjoy!
--- install.sh
+++ install.sh
@@ -21,16 +21,6 @@ protontricks-launch --appid 377840 EchoS9.tmp.exe &> /dev/null
 rm EchoS9.tmp.exe
 echo
 
-echo "Forcing Final Fantasy IX to run under Proton Experimental"
-kill $(ps aux | grep '[s]team -steamdeck' | awk '{print $2}')
-sleep 10
-cp ${HOME}/.local/share/Steam/config/config.vdf ${HOME}/.local/share/Steam/config/config.vdf.bak
-perl -0777 -i -pe 's/"CompatToolMapping"\n\s+{/"CompatToolMapping"\n\t\t\t\t{\n\t\t\t\t\t"377840"\n\t\t\t\t\t{\n\t\t\t\t\t\t"name"\t\t"proton_experimental"\n\t\t\t\t\t\t"config"\t\t""\n\t\t\t\t\t\t"priority"\t\t"250"\n\t\t\t\t\t}/gs' \
-${HOME}/.local/share/Steam/config/config.vdf
-# Thanks ChatGPT
-nohup steam &> /dev/null &
-echo
-
 echo "Done! Launch Final Fantasy IX to select mods."
 echo "Press enter to exit."
 read
